9aae244585d30e56c66b1c3e4458bbb278b03ac8
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2022-12-23 18:31:18 +0200

    Initial Commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+const Header = () => {
+    return (
+        <header id='site-header'>
+            <nav className='navbar'>
+                <section className='navbar-dashboard'>
+                    {/* <Link to="/dashboard">Dashboard</Link>
+                    <div id='guest'>
+                        <Link className='button' to='/login'>
+                            Login
+                        </Link>
+                        <Link to='/register' className='button'>
+                            Register
+                        </Link>
+                    </div>
+                    <div id='user'>
+                        <span>Welcome, email</span>
+                        <Link className='button' to='/my-pets'>
+                            My Pets
+                        </Link>
+                        <Link className='button' to='/create'>
+                            Add Pet
+                        </Link>
+                        <Link className='button' to='/logout'>
+                            Logout
+                        </Link>
+                    </div> */}
+                </section>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
