47a223597796aae8b15776697fa54642a2bff4d6
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-01-15 22:13:05 +0200

    made PetList reusable

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,20 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import * as petService from '../../services/petService';
 import PetList from '../PetList';
 
 const Dashboard = () => {
+    const [pets, setPets] = useState([]);
+
+    useEffect(() => {
+        petService.getAll().then((result) => setPets(result));
+    }, []);
+
     return (
         <section id='dashboard-page' className='dashboard'>
             <h1>Dashboard</h1>
             <section>
-                <Routes>
-                    <Route path='/' element={<PetList />} />
-                </Routes>
+                <PetList pets={pets}/>
             </section>
         </section>
     );
--- src/components/PetList/PetList.js
+++ src/components/PetList/PetList.js
@@ -2,18 +2,19 @@ import PetCard from './PetCard';
 import { useEffect, useState } from 'react';
 import * as petService from '../../services/petService';
 
-const PetList = () => {
-    const [pets, setPets] = useState([]);
-
-    useEffect(() => {
-        petService.getAll().then((result) => setPets(result));
-    }, []);
+const PetList = ({ pets }) => {
     return (
-        <ul className='other-pets-list'>
-            {pets.length ? pets.map((pet) => (
-                <PetCard key={pet._id} pet={pet} />
-            )) : <p>No pets in database!</p>}
-        </ul>
+        <>
+            {pets.length > 0 ? (
+                <ul className='other-pets-list'>
+                    {pets.map((pet) => (
+                        <PetCard key={pet._id} pet={pet} />
+                    ))}
+                </ul>
+            ) : (
+                <p className='no-pets'>No pets in database!</p>
+            )}
+        </>
     );
 };
 
