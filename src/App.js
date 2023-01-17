d1c3b7d6e9ff3f071477e0fe3034d767bbbef21a
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-01-17 22:12:08 +0200

    Error Boundary added

--- src/App.js
+++ src/App.js
@@ -11,30 +11,33 @@ import Logout from './components/Logout';
 import Edit from './components/Edit/Edit';
 import GuardedRoute from './components/Common/GuardedRoute';
 import PrivateRoute from './components/Common/PrivateRoute';
+import ErrorBoundary from './components/Common/ErrorBoundary';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
     return (
-        <AuthProvider>
-            <div id='container'>
-                <Header />
-                <main id='site-content'>
-                    <Routes>
-                        <Route path='/*' element={<Dashboard />} />
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/logout' element={<Logout />} />
-                        <Route path='/register' element={<Register />} />
-                        <Route path='/my-pets' element={<PrivateRoute><MyPets /></PrivateRoute>} />
-                        <Route path='/details/:petId' element={<Details />} />
+        <ErrorBoundary>
+            <AuthProvider>
+                <div id='container'>
+                    <Header />
+                    <main id='site-content'>
+                        <Routes>
+                            <Route path='/*' element={<Dashboard />} />
+                            <Route path='/login' element={<Login />} />
+                            <Route path='/logout' element={<Logout />} />
+                            <Route path='/register' element={<Register />} />
+                            <Route path='/my-pets' element={<PrivateRoute><MyPets /></PrivateRoute>} />
+                            <Route path='/details/:petId' element={<Details />} />
 
-                        <Route element={<GuardedRoute />}>
-                            <Route path='/create' element={<Create />} />
-                            <Route path='/edit/:petId' element={<Edit />} />
-                        </Route>
-                    </Routes>
-                </main>
-            </div>
-        </AuthProvider>
+                            <Route element={<GuardedRoute />}>
+                                <Route path='/create' element={<Create />} />
+                                <Route path='/edit/:petId' element={<Edit />} />
+                            </Route>
+                        </Routes>
+                    </main>
+                </div>
+            </AuthProvider>
+        </ErrorBoundary>
     );
 }
 
